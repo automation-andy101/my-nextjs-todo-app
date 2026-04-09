80f869bd992f1ecd6b06c7941d82575bb1c624f5
Andy <automation.andy101@gmail.com>
2026-04-09 21:08:24 +0100

    Added navbar component

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,8 +1,16 @@
+"use client"
+
 import { CheckSquare } from "lucide-react";
 import Link from "next/link";
 import { Button } from "./ui/button";
+import { DropdownMenu, DropdownMenuContent, DropdownMenuItem, DropdownMenuLabel, DropdownMenuTrigger } from "./ui/dropdown-menu";
+import { Avatar, AvatarFallback } from "./ui/avatar";
+import SignOutButton from "./sign-out-btn";
+import { useSession } from "@/lib/auth/auth-client";
 
 export default function Navbar() {
+    const {data: session } = useSession();
+    
     return (
         <nav className="border-b border-gray-200 bg-white">
             <div className="container items-center mx-auto flex h-16 px-4 justify-between">
@@ -11,12 +19,47 @@ export default function Navbar() {
                     Todo App
                 </Link>
                 <div className="flex items-center gap-4">
-                    <Link href="/sign-in">
-                        <Button variant="ghost" className="text-gray-700 hover:text-black">Login</Button>
-                    </Link>
-                    <Link href="/sign-up">
-                        <Button className="bg-primary hover:bg-primary/90">Start for free</Button>
-                    </Link>
+                    {session?.user ? (
+                        <>
+                            <DropdownMenu>
+                                <DropdownMenuTrigger asChild>
+                                    <Button 
+                                        variant="ghost"
+                                        className="relative h-8 w-8 rounded-full"    
+                                    >
+                                        <Avatar className="h-8 w-8">
+                                            <AvatarFallback className="bg-primary text-white">
+                                                {session.user.name[0].toLocaleUpperCase()}
+                                            </AvatarFallback>
+                                        </Avatar>
+                                    </Button>
+                                </DropdownMenuTrigger>
+
+                                <DropdownMenuContent className="w-56" align="end">
+                                    <DropdownMenuLabel className="font-normal">
+                                        <div className="flex flex-col space-y-1">
+                                            <p
+                                                className="text-sm font-medium leading-none"
+                                            >
+                                                {session.user.name}
+                                            </p>
+                                            <p className="text-xs leading-none text-muted-foreground">{session.user.email}</p>
+                                        </div>
+                                    </DropdownMenuLabel>
+                                    <SignOutButton />
+                                </DropdownMenuContent>
+                            </DropdownMenu>
+                        </>
+                    ) : (
+                        <>
+                            <Link href="/sign-in">
+                                <Button variant="ghost" className="text-gray-700 hover:text-black">Login</Button>
+                            </Link>
+                            <Link href="/sign-up">
+                                <Button className="bg-primary hover:bg-primary/90">Start for free</Button>
+                            </Link>
+                        </>
+                    )}
                 </div>
             </div>
         </nav>
